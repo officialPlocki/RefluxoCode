271fb0ac5697a0360c7c7187ac245f3cb3c97bd0
officialplocki <ko0l9999>
2021-12-05 22:54:32 +0100

    initial release

---
+++ server-libary/src/main/java/me/refluxo/serverlibary/ServerLibary.java
@@ -0,0 +1,17 @@
+package me.refluxo.serverlibary;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerLibary extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
